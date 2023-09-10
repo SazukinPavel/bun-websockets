cf032ba2d7855b2468f90d941833816106304597
SazukinPavel <sazukinpavelal@gmail.com>
2023-09-10 23:09:43 +0400

    add simple recieving message

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  port: 3000,
+  fetch(req, server) {
+    if (server.upgrade(req)) {
+      return;
+    }
+    return new Response("Upgrade failed :(", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      ws.send(`From server ${message}`);
+    },
+    open(ws) {
+      console.log("socket open");
+    },
+    close(ws, code, message) {
+      console.log("socket close");
+    },
+  },
+});
