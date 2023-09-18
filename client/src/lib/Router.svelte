f3d2efd62469388ac4cedefbfdc52f494bb4f082
SazukinPavel <sazukinpavelal@gmail.com>
2023-09-19 01:03:42 +0400

    add simple routing

---
+++ client/src/lib/Router.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import Login from "../views/Login.svelte";
+  import Chat from "../views/Chat.svelte";
+</script>
+
+<main>
+  <Router>
+    <Route path="/chat">
+      <Chat />
+    </Route>
+    <Route path="/">
+      <Login />
+    </Route>
+  </Router>
+</main>
